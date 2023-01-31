b76a53f1662debea461fe7f5a41e5c0e0d790034
Ivan <hanhtetsan13@gmail.com>
2023-01-31 16:14:48 +0630

    add

--- Components/Welcome/Welcome.jsx
+++ Components/Welcome/Welcome.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import styles from "./Welcome.module.css"
+
+
 
 const Welcome = () => {
-  return (
-    <div>Welcome</div>
-  )
+    return (
+        <div className={styles.welcome}>
+            <div className={styles.title}>
+                Welcome from the Cygnus Decentralized Chat Application...
+            </div>
+            <div className={styles.info}>
+                You'll be wondering how this app works and how it differs from other chat apps.
+            </div>
+        </div>
+    )
 }
 
 export default Welcome
